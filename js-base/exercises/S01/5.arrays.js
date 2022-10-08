9cf19c2dbf2b9389812c9ad1be279ccfafc8d84d
Jufinka <justynafijor@gmail.com>
2022-10-08 17:15:58 +0200

    all solutions for section-1

---
+++ js-base/exercises/S01/5.arrays.js
@@ -0,0 +1,25 @@
+// Your task:
+// 1. Create new array with 3 elements: 'abc', 'def' and 'gh'
+// 2. Print on console whole array
+// 3. Print on console second ([1]) element from array
+// 4. Create readable names for variables and function
+
+// to test your solution in terminal You can run following command:
+// npm run es1e5
+
+//// TODO:
+// here place your solution:
+
+const stringArray = ['abc', 'def', 'gh']
+console.log(stringArray)
+console.log(stringArray[1])
+console.log(...stringArray)
+
+
+//// DONT MODIFY CODE BELOW!
+// Here You will find expected result of exercise
+
+// Expected output:
+// On console You should get:
+// ['abc', 'def', 'gh']
+// 'def'
